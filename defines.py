be3a07fcd92beff119a2e7e43801f2afdab90071
olavosamp <olavosamp@poli.ufrj.br>
2018-06-07 16:47:30 -0300

    Preliminary results. Updated train and test sets.

--- defines.py
+++ defines.py
@@ -7,9 +7,9 @@ standardSample = 17
 
 # Dataset sample size
 # fracPos == fracNeg yields an uniformly sampled dataset
-fracPos  = 0.02 #  2 %
-fracNeg  = 0.02 #  2 %
-fracTest = 0.2  # 20 %
+fracPos  = 0.03 #  3 %
+fracNeg  = 0.03 #  3 %
+fracTest = 0.3  # 30 %
 
 # Function success value
 success = 0
